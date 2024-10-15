1e1131ee62357535f8290ec60b88da71108711ce
Dana Cimpoaca <dana.cimpoaca@smartsoftdev.eu>
2024-10-15 22:24:46 +0300

    Added the Variables and Comments task

---
+++ src/main/java/com/danacimpoacaqa/variableandcomments/VariablesAndComments.java
@@ -0,0 +1,43 @@
+package com.danacimpoacaqa.variableandcomments;
+
+public class VariablesAndComments {
+
+    public static void main(String[] args) {
+
+//        Declare the primitive data type variable
+
+        boolean falseBooleanVariable;
+        boolean trueBooleanVariable;
+        char firstCharFromAscii;
+        char lastCharFromAscii;
+        byte lowerLimit0fByteDateType;
+        byte upperLimitOfByteDateType;
+        short lowerLimit0fShortDataType;
+        short upperLimitOfShortDataType;
+        int lowerLimitOfIntDataType;
+        int upperLimitOfIntDataType;
+        long lowerLimitOfLongDataType;
+        long upperLimitOgLongDataType;
+        float lowerLimitOfFloatDataType;
+        float upperLimitOfFloatDataType;
+        double lowerLimitOfDoubleDataType;
+        double upperLimitOfDoubleDataType;
+
+ //   Initialization of the above variable
+
+        falseBooleanVariable = false;
+        trueBooleanVariable = true;
+        lowerLimit0fByteDateType = -128;
+        upperLimitOfByteDateType = 127;
+        lowerLimit0fShortDataType = -32768;
+        upperLimitOfShortDataType = 32767;
+        lowerLimitOfIntDataType = - 2147483648;
+        upperLimitOfIntDataType = 2147483647;
+        lowerLimitOfLongDataType = -9223372036854775808L;
+        upperLimitOgLongDataType = 9223372036854775807L;
+        lowerLimitOfFloatDataType = -3.4E+38f;
+        upperLimitOfFloatDataType = 3.4E+38f;
+        lowerLimitOfDoubleDataType = -1.7E+308d;
+        upperLimitOfDoubleDataType = 1.7E+305;
+    }
+}
